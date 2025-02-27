855544f1b4c4b5fbf3dc3c5cd2c9bb85d8f96797
sandjian <107805130+sandjian@users.noreply.github.com>
2025-02-27 16:59:54 -0300

    the last commit

--- components/Sections/topTags.tsx
+++ components/Sections/topTags.tsx
@@ -1,7 +1,10 @@
+// components/TopTags.tsx
+import { Suspense } from "react";
 import { getTopTags } from "@/lib/api";
+import Link from "next/link";
 
 export default async function TopTags() {
-  const popularTags = await getTopTags(14); // Obtener los 25 tags más populares
+  const popularTags = await getTopTags(14);
 
   return (
     <section className="w-full max-w-7xl mx-auto px-4">
@@ -12,18 +15,35 @@ export default async function TopTags() {
         </h2>
       </div>
 
-      <div className="rounded-lg overflow-hidden">
-        <div className="flex flex-wrap gap-3">
-          {popularTags.map((tag, index) => (
-            <div
-              key={index}
-              className="bg-neutral-200 dark:bg-neutral-800 px-4 py-3 rounded-2xl text-foreground font-medium hover:bg-primary dark:hover:bg-primary hover:text-primary-foreground transition-colors duration-200 cursor-pointer"
-            >
-              #{tag.toLowerCase()}
-            </div>
-          ))}
+      <Suspense fallback={<SkeletonTopTags />}>
+        <div className="rounded-lg overflow-hidden">
+          <div className="flex flex-wrap gap-3">
+            {popularTags.map((tag, index) => (
+              <Link href={`/tags/${tag.toLowerCase()}`} key={index}>
+                <div className="bg-neutral-200 dark:bg-neutral-800 px-4 py-3 rounded-2xl text-foreground font-medium hover:bg-primary dark:hover:bg-primary hover:text-primary-foreground transition-colors duration-200 cursor-pointer">
+                  #{tag.toLowerCase()}
+                </div>
+              </Link>
+            ))}
+          </div>
         </div>
-      </div>
+      </Suspense>
     </section>
+  );
+}
+
+// Skeleton para TopTags
+function SkeletonTopTags() {
+  return (
+    <div className="rounded-lg overflow-hidden animate-pulse">
+      <div className="flex flex-wrap gap-3">
+        {[...Array(14)].map((_, i) => (
+          <div
+            key={i}
+            className="bg-gray-300 dark:bg-gray-700 px-4 py-3 rounded-2xl h-10 w-24"
+          ></div>
+        ))}
+      </div>
+    </div>
   );
 }
